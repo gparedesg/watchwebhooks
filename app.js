31f012c00b4148810d085417eb27c3e0fed85511
Gabriel Paredes <gabriel@axented.com>
2023-02-03 19:04:38 -0600

    meh

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import { createServer } from 'http';
 
-const hostname = '127.0.0.1';
-const port = 8080;
+const hostname = 'localhost';
+const port = 3000;
 
 const server = createServer((req, res) => {
   res.statusCode = 200;
